08436b46401a4bb1ec306dc6981b06f133406a3d
SusheelKumaraswamy <susi31398@gmail.com>
2022-05-12 22:00:41 +0530

    first commit

--- database.js
+++ database.js
@@ -3,8 +3,9 @@ const {MongoClient}=require('mongodb');
      selectedDb:{},
      async connect(){
          try {
-            const client= await MongoClient.connect("mongodb://localhost:27017");
+            const client= await MongoClient.connect("mongodb+srv://SusheelKumaraswamy:rIKYqMGFJyVRIQl7@cluster0.leiii.mongodb.net?retryWrites=true&w=majority");
             this.selectedDb=client.db('hallbooking');
+            console.log(this.selectedDb);
          } 
          catch (error) {
              console.log(error);
